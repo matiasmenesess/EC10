2d01a27f8788b6a4face187956102ad7f047e5ae
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-23 21:11:31 -0500

    Update problema_2.cpp

--- problema_2.cpp
+++ problema_2.cpp
@@ -5,169 +5,123 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-template <typename T1, typename T2>
-struct key_value {
-    T1 key = T1();
-    T2 value = T2();
+#define ALPHABET_SIZE 26
+
+// Nodo del Trie
+struct TrieNode {
+    TrieNode* children[ALPHABET_SIZE];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        // Inicializar todos los hijos con nullptr
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            children[i] = nullptr;
+        }
+    }
 };
 
-template <typename T1, typename T2>
-class Hash_Table {
-    int current_size;
-    int element_count;
-    key_value<T1, T2>* table;
+class Trie {
+private:
+    TrieNode* root;
+
 
 public:
-    Hash_Table(int initial_size) {
-        table = new key_value<T1, T2>[initial_size];
-        current_size = initial_size;
-        element_count = 0;
+    Trie() {
+        root = new TrieNode();
     }
 
-    int hash_function(const string& key) { //funcion hash para strings
-        unsigned hash = 5381;
-        for (char c : key) {
-            hash = ((hash << 5) + hash) + c;
-        }
-        return hash % current_size;
-    }
-
-    void insert(T1 key, T2 value) {
-        int index = hash_function(key);
-        while (table[index].key != T1() && table[index].key != key) {
-            index = (index + 1) % current_size;
-        }
-        if (table[index].key == key) {
-            table[index].value += value;
-        } else {
-            table[index].key = key;
-            table[index].value = value;
-            element_count++;
-        }
-    }
-
-    T2* search(T1 key) {
-        int index = hash_function(key);
-        while (table[index].key != T1()) {
-            if (table[index].key == key) {
-                return &table[index].value;
+    // Insertar una palabra en el Trie
+    void insert(const string& word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';  // Convertir el carácter a índice (0-25)
+            if (node->children[index] == nullptr) {
+                node->children[index] = new TrieNode();
             }
-            index = (index + 1) % current_size;
+            node = node->children[index];
         }
-        return nullptr;
+        node->isEndOfWord = true;  // Marcar el fin de la palabra
     }
 
-    key_value<T1, T2>* begin() { return &table[0]; } //para iterar
-    key_value<T1, T2>* end() { return &table[current_size]; } //para iterar
-
-    // ~Hash_Table() {
-    //     delete[] table;
-    // }
-};
-
-template<typename T>
-class Heap {
-    string type;
-    vector<T> elements;
-
-    void heapify_down(int i) {
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-        int largest = i;
-
-        if (type == "MAX_HEAP") {
-            if (left < elements.size() && Comparar(elements[left], elements[largest])) {
-                largest = left;
+    // Buscar una palabra exacta en el Trie
+    bool search(const string& word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                return false;  // La palabra no existe
             }
-            if (right < elements.size() && Comparar(elements[right], elements[largest])) {
-                largest = right;
+            node = node->children[index];
+        }
+        return node->isEndOfWord;  // Verificar si es el fin de la palabra
+    }
+
+    // Buscar si un prefijo existe en el Trie
+    bool startsWith(const string& prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                return false;  // El prefijo no existe
+            }
+            node = node->children[index];
+        }
+        return true;  // El prefijo existe
+    }
+
+    vector<string> palabras_con_prefijo(const string& prefix, int npalabras) {
+        TrieNode* node = root;
+        vector<string> resultado;
+        for (char ch : prefix) {
+            int index = ch - 'a';
+            if (node->children[index] == nullptr) {
+                return resultado;
+            }
+            node = node->children[index];
+        }
+        palabras(node, prefix, resultado, npalabras);
+        return resultado;
+    }
+
+    void palabras(TrieNode* node, string palabra, vector<string>& result, int npalabras) {
+        if (result.size() >= npalabras) return;
+        if (node->isEndOfWord) {
+            result.push_back(palabra);
+        }
+
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (node->children[i] != nullptr) {
+                char nextChar = 'a' + i;
+                palabras(node->children[i], palabra + nextChar, result, npalabras);
             }
         }
-
-        if (largest != i) {
-            swap(elements[i], elements[largest]);
-            heapify_down(largest);
-        }
     }
 
-    void heapify_up(int i) {
-        int parent = (i - 1) / 2;
-
-        if (i && Comparar(elements[i], elements[parent])) {
-            swap(elements[i], elements[parent]);
-            heapify_up(parent);
-        }
-    }
-
-    bool Comparar(const T& a, const T& b) {
-        if (a.first == b.first) {
-            return a.second < b.second;  // orden lexicografico
-        }
-        return a.first > b.first;  // mayor frecuencia primero
-    }
-
-public:
-    Heap(string heapType) : type(heapType) {}
-
-    void insert(T val) {
-        elements.push_back(val);
-        heapify_up(elements.size() - 1);
-    }
-
-    void pop() {
-        if (elements.size() == 0) {
-            cout << "Heap vacío" << endl;
-            return;
-        }
-
-        swap(elements[0], elements[elements.size() - 1]);
-        elements.pop_back();
-        heapify_down(0);
-    }
-
-    T top() {
-        if (elements.size() == 0) {
-            // throw out_of_range("Heap vacio");
-        }
-        return elements[0];
-    }
-
-    int size() {
-        return elements.size();
-    }
 };
 
 class Solution {
 public:
-    vector<string> topKFrequent(vector<string>& words, int k) {
-        Hash_Table<string, int> HT(words.size());
+    vector<vector<string>> suggestedProducts(vector<string>& products, string searchWord) {
+        sort(products.begin(), products.end());
 
-        for (const string& word : words) {
-            int* freq = HT.search(word);
-            if (freq) {
-                (*freq)++;
-            } else {
-                HT.insert(word, 1);
-            }
+        Trie trie;
+        for (const string& product : products) {
+            trie.insert(product);
         }
 
-        Heap<pair<int, string>> maxHeap("MAX_HEAP");
-
-        for (auto it = HT.begin(); it != HT.end(); ++it) {
-            if (it->key != "") {
-                maxHeap.insert({it->value, it->key});
-            }
+        vector<vector<string>> resultado;
+        string prefix = "";
+        for (char ch : searchWord) {
+            prefix += ch;
+            vector<string> palabras = trie.palabras_con_prefijo(prefix, 3);
+            resultado.push_back(palabras);
         }
 
-        vector<string> result;
-        for (int i = 0; i < k; ++i) {
-            result.push_back(maxHeap.top().second);
-            maxHeap.pop();
-        }
-
-        return result;
+        return resultado;
     }
 };
 
